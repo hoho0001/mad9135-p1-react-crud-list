29d39c5a8cfabe218e711013c5d86fa6c2d55608
HoHoangLien <hoho0001@algonquinlive.com>
2019-10-30 09:55:37 -0400

    Updates

---
+++ precache-manifest.d49091b18fbc7d50688c4225042cee2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88cd582c93a95e90d7a1538a9f4f739a",
+    "url": "/mad9135-p1-react-crud-list/index.html"
+  },
+  {
+    "revision": "5a5cbefa21cb7ed59832",
+    "url": "/mad9135-p1-react-crud-list/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0076524141ea9ff3debb",
+    "url": "/mad9135-p1-react-crud-list/static/js/2.82809dbe.chunk.js"
+  },
+  {
+    "revision": "5a5cbefa21cb7ed59832",
+    "url": "/mad9135-p1-react-crud-list/static/js/main.809ed6f5.chunk.js"
+  },
+  {
+    "revision": "e66bb050bf0bb8495a3a",
+    "url": "/mad9135-p1-react-crud-list/static/js/runtime-main.79f10162.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/mad9135-p1-react-crud-list/static/media/logo.25bf045c.svg"
+  }
+]);
